1fbf27f88ddc15591f59e612f66adfa3b66aecbb
Mallo <henrietma@gmail.com>
2025-04-29 01:09:41 +0200

    add sources for generator

--- test-db/querie_gen.py
+++ test-db/querie_gen.py
@@ -1,5 +1,16 @@
 # Generate interesting SQL queries
 
+# Will use the techniques found in the resources of the instructions
+
+# Pivoted Query Synthesis : https://www.youtube.com/watch?v=0aeDyXgzo04
+
+# Query Partitioning : https://dl.acm.org/doi/10.1145/3428279
+
+# Query Plan Guidance : https://ieeexplore.ieee.org/document/10172874
+
+# Equivalent Expression Transformation : https://www.usenix.org/system/files/osdi24-jiang.pdf
+
+
 import random
 
 class QueryGenerator:
