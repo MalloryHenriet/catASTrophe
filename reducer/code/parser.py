80ad7253e5d81459985ebce9d03b8833613ae8fa
Mallo <henrietma@gmail.com>
2025-06-07 13:45:06 +0200

    tokenization

--- reducer/code/parser.py
+++ reducer/code/parser.py
@@ -1,6 +1,5 @@
-import sqlglot
 import sqlparse
-
+from sqlparse.tokens import Token
 class SQLParser:
     def __init__(self, dialect="sqlite"):
         self.dialect = dialect
@@ -8,22 +7,16 @@ class SQLParser:
     # Take a SQL string and return the syntax tree
     def parse(self, sql_lines):
         sql_string = "".join(sql_lines)
-        parsed_stmt = []
-
-        for stmt in sqlparse.split(sql_string):
-            stmt = stmt.strip()
-            if not stmt:
-                continue
-            try:
-                ast = sqlglot.parse_one(stmt, self.dialect)
-                parsed_stmt.append(ast)
-            except Exception as e:
-                print(f"Parsing error: {e}")
-        return parsed_stmt
+        statements = [stmt.strip() for stmt in sqlparse.split(sql_string) if stmt.strip()]
+        tokenized_statements = [sqlparse.parse(stmt)[0] for stmt in statements]
+        return tokenized_statements
     
     # Take the syntax tree back to SQL string
-    def to_sql(self, ast):
-        return ";\n".join(expr.sql() for expr in ast if expr is not None) + ";"
+    def to_sql(self, token_trees):
+        return "\n".join(str(tree).strip() for tree in token_trees if tree is not None) + ";"
+
+    def flatten_tokens(self, token_tree):
+        return [token for token in token_tree.flatten() if token.ttype != Token.Text.Whitespace]
 
 
 if __name__ == "__main__":
