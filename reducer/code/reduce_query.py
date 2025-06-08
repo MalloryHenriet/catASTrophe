8d1c840ee5822e6f3a3bb13360d6e9c45e3d5190
Sara Jun <sarajun@Saras-MacBook-Pro.local>
2025-06-08 22:58:38 +0100

    removing first where

--- reducer/code/reduce_query.py
+++ reducer/code/reduce_query.py
@@ -1,14 +1,19 @@
 from code.parser import SQLParser
 from code.executor import execute_query
 from code.delta_debugging import delta_debugging
+from code.semantic import reduce_where_clause
 
 def reduce_query(query_path, test_script, output_path):
     with open(f"{query_path}/original_test.sql", "r") as original_query:
-        query_string = original_query.readlines()
+        query_string = original_query.read()
+
+
+    reduced_query = reduce_where_clause(query_string, test_script, output_path)
+    print("reduced_query ", reduced_query)
 
     # Parse the query to an AST
     parser = SQLParser()
-    ast_list = parser.parse(query_string)
+    ast_list = parser.parse([reduced_query])
 
     if not ast_list:
         print("No valid statement to reduce")
@@ -21,8 +26,8 @@ def reduce_query(query_path, test_script, output_path):
 
         return execute_query(query_string, test_script, output_path)
 
-    # Update AST with delta debugging technique
-    ast_list = delta_debugging(ast_list, validator)
+    # # Update AST with delta debugging technique
+    # ast_list = delta_debugging(ast_list, validator)
 
     minimized = parser.to_sql(ast_list)
 
---
+++ reducer/code/semantic.py
@@ -0,0 +1,39 @@
+import sqlparse
+from code.executor import execute_query
+
+
+def extract_where_expressions(where_clause):
+    return [expr.strip() for expr in where_clause.split("AND")]
+
+def rebuild_query(base_query, where_exprs):
+    if not where_exprs:
+        return base_query.split("WHERE")[0].strip() + ";"
+    new_where = " AND ".join(where_exprs)
+    return base_query.split("WHERE")[0].strip() + f" WHERE {new_where};"
+
+def reduce_where_clause(query_str, test_script, query_output_path="query.sql"):
+    if "WHERE" not in query_str.upper():
+        return query_str
+
+    where_start = query_str.upper().find("WHERE")
+    base_query = query_str[:where_start]
+    where_clause = query_str[where_start + len("WHERE"):].strip().rstrip(";")
+
+    expressions = extract_where_expressions(where_clause)
+    i = 0
+
+    while i < len(expressions):
+        trial_exprs = expressions[:i] + expressions[i+1:]
+        trial_query = rebuild_query(base_query, trial_exprs)
+
+        result_code = execute_query(trial_query, test_script, query_output_path)
+
+        if result_code == 0:
+            print(f"[REDUCED] Removed WHERE clause: {expressions[i]}")
+            expressions = trial_exprs
+        else:
+            print(f"[RETAINED] Keeping WHERE clause: {expressions[i]}")
+            i += 1
+
+    return rebuild_query(base_query, expressions)
+
